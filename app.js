9fc8fa69896c8a07ddbe67b2765bf07ea96f42b2
Isaiah J Turner <github.com@isaiahjturner.com>
2016-01-23 19:57:33 -0500

    new code

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
 var express = require('express');
 var Patient = require("./models/Patient");
 var bodyParser = require('body-parser');
+var ejs = require('ejs');
 
 var app = express();
-var mongoURL = " mongodb://kangoobear:password@ds041934.mongolab.com:41934/kangoobear";
+var mongoURL = "mongodb://kangoobear:password@ds041934.mongolab.com:41934/kangoobear";
 
 app.set('port', (process.env.PORT || 6969));
+app.use(express.static(__dirname + '/public'))
 
 app.use(bodyParser.json({
   type: ['application/json', 'application/vnd.api+json']
-}))
+}));
+
+app.set('view engine', 'ejs');
 
 app.get("/", function(req, res) {
+  res.render("login");
+});
+
+app.get("/api", function(req, res) {
   res.json({
     goodCode: false
   })
 });
 
-app.get("/patients", function(req, res) {
+app.get("/api/patients", function(req, res) {
   Patient.find(function(err, patients) {
     res.json({
       err: err,
@@ -26,7 +34,7 @@ app.get("/patients", function(req, res) {
   })
 });
 
-app.post("/patients", function(req, res) {
+app.post("/api/patients", function(req, res) {
   res.send(req.body);
   var patient = new Patient(req.body.modelData);
   patient.save(function(err, patient) {
@@ -37,6 +45,14 @@ app.post("/patients", function(req, res) {
   })
 });
 
+app.get("/reset", function(req, res) {
+  Patient.remove({}, function(err, updated) {
+    res.json({
+      err: err,
+      updated: updated
+    })
+  })
+})
 app.listen(app.get('port'), function() {
   console.log('Started!')
 });
--- models/Patient.js
+++ models/Patient.js
@@ -12,6 +12,15 @@ var PatientSchema = new Schema({
   name: {
     type: String
   },
+  dosage: {
+    type: String
+  },
+  phone: {
+    type: String
+  },
+  notes: {
+    type: String
+  },
   address: {
     type: String
   },
@@ -26,6 +35,12 @@ var PatientSchema = new Schema({
 PatientSchema.pre('save', function(next) {
   now = new Date();
   this.updatedAt = now;
+  if (this.phone && this.phone.length === 10) {
+    var first = this.phone.substring(0, 3);
+    var second = this.phone.substring(3, 6);
+    var third = this.phone.substring(6, 10);
+    this.phone = first + "-" + second + "-" + third;
+  }
   if (!this.createdAt) {
     this.createdAt = now;
   }
